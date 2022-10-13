3e0219519b135db5e893cff63292c07fe5dbcb60
Thuan <vanthuan30102k@gmail.com>
2022-10-13 17:54:31 +0700

    add

---
+++ public/js/detail.js
@@ -0,0 +1,7 @@
+import $ from "jquery";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick.min.js";
+import "jquery/dist/jquery.min.js";
+import "../css/style.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
