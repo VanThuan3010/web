761ef1d6ec37888dfdefe6bae8043338957b70c2
Thuan <vanthuan30102k@gmail.com>
2022-10-20 21:02:24 +0700

    a

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,11 @@ export default defineConfig({
       input: {
         index: resolve(__dirname, "index.html"),
         shop: resolve(__dirname, "shop.html"),
+        about: resolve(__dirname, "aboutus.html"),
+        blog: resolve(__dirname, "blog.html"),
+        cart: resolve(__dirname, "cart.html"),
+        checkout: resolve(__dirname, "checkout.html"),
+        detail: resolve(__dirname, "detail.html"),
       },
     },
   },
