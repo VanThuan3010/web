47dc97d8143bdc02b89a7974a7ab86786ae2ea9b
Thuan <vanthuan30102k@gmail.com>
2022-10-20 18:06:53 +0700

    respon about shop

--- public/js/index.js
+++ public/js/index.js
@@ -83,6 +83,29 @@ $(".project-timeline-slide").slick({
   autoplay: true,
   autoplaySpeed: 2000,
   arrows: false,
+  responsive: [
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 });
 
 $(".said-slide").slick({
@@ -95,6 +118,29 @@ $(".said-slide").slick({
   autoplay: true,
   autoplaySpeed: 4000,
   arrows: false,
+  responsive: [
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 });
 
 $(".count").each(function () {
