51b642409bb06c7c312b93225efb01a389c93631
Thuan <vanthuan30102k@gmail.com>
2022-10-27 02:30:05 +0700

    nsgjf

--- public/js/shop.js
+++ public/js/shop.js
@@ -2,6 +2,46 @@ import $ from "jquery";
 import _ from "lodash";
 import { products } from "./product";
 
+const filter = () => {
+  const categories = [];
+  let value = $("#sort-by").val();
+  let filterProducts;
+  let clone = [];
+  $("input:checked").each(function () {
+    categories.push(this.value);
+  });
+  let min = $(".min-price").val();
+  let max = $(".max-price").val();
+  if (min == "") min = 0;
+  if (max != "") {
+    filterProducts = products.filter(
+      (p) =>
+        (categories.length === 0 || categories.includes(p.category)) &&
+        ( p.price >= min && p.price <= max)
+    );
+  } else
+    filterProducts = products.filter(
+      (p) =>
+        (categories.length === 0 || categories.includes(p.category)) &&
+        ( p.price >= min)
+    );
+  for (let i = 0; i < filterProducts.length; i++) {
+    clone[i] = filterProducts[i];
+  }
+  if (value == "sort-by-ascending") {
+    let sortProduct = clone.sort((a, b) => a.price - b.price);
+    render(sortProduct);
+  }
+  if (value == "sort-by-decreasing") {
+    let sortProduct = clone.sort((a, b) => b.price - a.price);
+    render(sortProduct);
+  }
+  if (value == "default") {
+    render(filterProducts);
+  }
+};
+
+
 const render = (event) => {
   const $productList = $(".list-pro");
   const productTemplate = $("#productTemplate").html();
@@ -20,49 +60,49 @@ const render = (event) => {
 };
 // filter
 const filterByCategory = (event) => {
-  const categories = [];
-  $("input:checked").each(function () {
-    categories.push(this.value);
-  });
-  const filterProducts = products.filter(
-    (p) => categories.length === 0 || categories.includes(p.category)
-  );
-  render(filterProducts);
+  // const categories = [];
+  // $("input:checked").each(function () {
+  //   categories.push(this.value);
+  // });
+  // let filterProducts = products.filter(
+  //   (p) => categories.length === 0 || categories.includes(p.category)
+  // );
+  // render(filterProducts);
+  filter();
 };
 
+
 const filterByPrice = (event) => {
-  let min = $(".min-price").val();
-  let max = $(".max-price").val();
-  let filterProducts;
-  if(min == "") min = 0;
-  if (max != "")
-  {filterProducts = products.filter(
-    (p) => p.price >= min && p.price <= max
-  );}
-  else {
-    filterProducts = products.filter(
-      (p) => p.price >= min
-    );
-  }
-  render(filterProducts);
+  // let min = $(".min-price").val();
+  // let max = $(".max-price").val();
+  // let filterProducts;
+  // if(min == "") min = 0;
+  // if (max != "")
+  // {filterProducts = products.filter(
+  //   (p) => p.price >= min && p.price <= max
+  // );}
+  // else {
+  //   filterProducts = products.filter((p) => p.price >= min);
+  // }
+  // render(filterProducts);
+  filter();
 }
 
 const sort = (event) => {
-  let value = $("#sort-by").val();
-  let clone = [];
-  for (let i = 0; i < products.length; i++) {
-    clone[i] = products[i];
-  }
-  if (value ==  "sort-by-ascending"){
-  let sortProduct = clone.sort((a,b) => a.price - b.price);
-  render(sortProduct);}
-  if (value == "sort-by-decreasing") {
-    let sortProduct = clone.sort((a, b) => b.price - a.price);
-    render(sortProduct);
-  }
-  if (value == "default") {
-    render(products);
-  }
+  // let value = $("#sort-by").val();
+  
+  // if (value == "sort-by-ascending") {
+  //   let sortProduct = clone.sort((a, b) => a.price - b.price);
+  //   render(sortProduct);
+  // }
+  // if (value == "sort-by-decreasing") {
+  //   let sortProduct = clone.sort((a, b) => b.price - a.price);
+  //   render(sortProduct);
+  // }
+  // if (value == "default") {
+  //   render(products);
+  // }
+  filter();
 };
 // add to cart
 const addToCart = (event) => {
@@ -97,8 +137,8 @@ $(() => {
       return dom;
     })
   );
-
   $("form.by-category").on("change", filterByCategory);
   $(".filter-btn").on("click", filterByPrice);
   $("#sort-by").on("change", sort);
+
 });
